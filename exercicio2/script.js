1b48a82cb582ccc83d9120c9b37cbac2a5599461
thiagogagliari <thiagogagliari@hotmail.com>
2022-07-18 23:29:08 -0300

    Resolução exercício 007

---
+++ exercicio2/script.js
@@ -0,0 +1,14 @@
+const perguntaA = "Você assiste futebol?"
+const respostaA = prompt(perguntaA)
+console.log(perguntaA)
+console.log(respostaA)
+
+const perguntaB = "Você gosta de pizza?"
+const respostaB = prompt(perguntaB)
+console.log(perguntaB)
+console.log(respostaB)
+
+const perguntaC = "Você gosta de fazer exercício físico?"
+const respostaC = prompt(perguntaC)
+console.log(perguntaC)
+console.log(respostaC)
---
+++ exercicio3/script.js
@@ -0,0 +1,10 @@
+let a = 25;
+let b = 10;
+let c;
+
+c=a
+a=b
+b=c
+
+console.log ('o novo valor de a é', a)
+console.log ('o novo valor de b é', b)
